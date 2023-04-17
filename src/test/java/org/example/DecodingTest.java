7caac9f5e6e4ea72879375c6c2d7230d19c000f6
zeus <zeuskeyz@gmail.com>
2023-04-17 13:54:46 +0300

    ammends code to the DecodingTest.java file to test f for decoding functionality.

--- src/test/java/org/example/DecodingTest.java
+++ src/test/java/org/example/DecodingTest.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodingTest {
-
     @Test
+    @DisplayName(value = "TESTING DECODING FUNCTIONS")
     void decode() {
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 23));
     }
 }
