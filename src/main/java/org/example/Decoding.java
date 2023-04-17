0f138d3598b2365099cc207d43e456887ad96621
zeus <zeuskeyz@gmail.com>
2023-04-17 13:35:42 +0300

    adds to the Decoding.java file, a function to decode the encoded message

--- src/main/java/org/example/Decoding.java
+++ src/main/java/org/example/Decoding.java
@@ -1,4 +1,31 @@
 package org.example;
-
 public class Decoding {
+    public static String decode (String message, int cipherKey) {
+
+        String decodingAlphabets = Alphabets.getEncryptors();;
+
+        //DECODING PART
+        String decodedText = "";
+        if (cipherKey > 0 && cipherKey < decodingAlphabets.length()){
+            for(int i=0; i<message.length(); i++){
+
+                Character msgLetter = message.charAt(i);
+
+                if(Character.isLetter(msgLetter)){
+
+                    int cipherStart = decodingAlphabets.length()- cipherKey + decodingAlphabets.indexOf(msgLetter);
+
+                    if (cipherStart < decodingAlphabets.length() )
+                        decodedText += decodingAlphabets.charAt(cipherStart);
+                    else
+                        decodedText += decodingAlphabets.charAt(cipherStart%decodingAlphabets.length());
+                }
+                else
+                    decodedText += msgLetter;
+            }
+        }
+        else
+            decodedText = "KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n";
+        return decodedText;
+    }
 }
