8da8dfd56b8b2e3932d59be9c353846177e9789a
zeus <zeuskeyz@gmail.com>
2023-04-17 14:08:10 +0300

    ammends code to the DecodingTest.java file to test for key error functionality if key is less than zero.

--- src/test/java/org/example/DecodingTest.java
+++ src/test/java/org/example/DecodingTest.java
@@ -11,5 +11,6 @@ class DecodingTest {
     void decode() {
         assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 23));
         assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 27));
+        assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 0));
     }
 }
