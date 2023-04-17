9987c60b763cde68c8be54d9fb9ed4106e7d1dcb
zeus <zeuskeyz@gmail.com>
2023-04-17 14:38:41 +0300

    ammends comments to the Main.java file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,43 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
+    //SETTER FUNCTION FOR THE USER MESSAGE TO BE ENCODED/DECODED
     private static String setMessage(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your message here: ");
         String message = scanner.nextLine().toUpperCase();
         return message;
     }
+
+    //SETTER FUNCTION FOR THE KEY TO BE USED TO ENCODE/DECODE USER MESSAGE
     private static Integer setKey(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the key ...(KEY SHOULD BE GREATER THAN 0 AND LESS THAN 26): ");
         int cipherKey = scanner.nextInt();
         return cipherKey;
     }
+
+    //GETTER FUNCTION FOR THE USER MESSAGE TO BE ENCODED/DECODED
     private static String getMessage(){
         return setMessage();
     }
+
+    //GETTER FUNCTION FOR THE KEY TO BE USED TO ENCODE/DECODE USER MESSAGE
     private static Integer getKey(){
         return setKey();
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         do {
+
+            //PROMPTS THE USER TO CHOOSE THE PROCESS THEY WISH TO EXECUTE
             System.out.print("What do you wish to do ...(E to ENCODE) or (D to DECODE): ");
             String process = scanner.nextLine();
 
+            //TELLS THE CODE WHICH FILE TO EXECUTE DEPENDING ON THE PROCESS SELECTED
             if (process.equalsIgnoreCase("E") || process.equalsIgnoreCase("ENCODE")) {
                 String output = Encoding.encode (getMessage(), getKey());
                 System.out.print("RESULT: ".concat(output));
@@ -42,9 +54,11 @@ public class Main {
                 continue;
             }
 
+            //PROMPTS THE USER WHETHER TO CONTINUE WITH OR TERMINATE THE PROCESS
             System.out.print("\nDo you wish to proceed...(C to CONTINUE) or (Q to Quit): ");
             String proceedPrompt = scanner.nextLine();
 
+            //OUTPUT IF THE USER TERMINATES THE PROCESS
             if (proceedPrompt.equalsIgnoreCase("Q") || proceedPrompt.equalsIgnoreCase("QUIT")){
                 System.out.println("PROCESS ENDED!");
                 break;
