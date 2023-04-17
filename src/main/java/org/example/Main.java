e036642ae6fc9c37135566f22c1b0f0b2a6a474e
zeus <zeuskeyz@gmail.com>
2023-04-17 13:40:33 +0300

    adds code to the Main.java file to handle the inputs and outpust from the decoding and encoding processes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,54 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+    private static String setMessage(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your message here: ");
+        String message = scanner.nextLine().toUpperCase();
+        return message;
+    }
+    private static Integer setKey(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the key ...(KEY SHOULD BE GREATER THAN 0 AND LESS THAN 26): ");
+        int cipherKey = scanner.nextInt();
+        return cipherKey;
+    }
+    private static String getMessage(){
+        return setMessage();
+    }
+    private static Integer getKey(){
+        return setKey();
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        do {
+            System.out.print("What do you wish to do ...(E to ENCODE) or (D to DECODE): ");
+            String process = scanner.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+            if (process.equalsIgnoreCase("E") || process.equalsIgnoreCase("ENCODE")) {
+                String output = Encoding.encode (getMessage(), getKey());
+                System.out.print("RESULT: ".concat(output));
+            }
+            else if (process.equalsIgnoreCase("D") || process.equalsIgnoreCase("DECODE")) {
+                String results = Decoding.decode(getMessage(), getKey());
+                System.out.print("RESULT: ".concat(results));
+                scanner.nextLine();
+            }
+            else {
+                System.out.print("PROCESS ERROR! PLEASE REPLY WITH (E TO ENCODE) OR (D TO DECODE)\n\n");
+                continue;
+            }
+
+            System.out.print("\nDo you wish to proceed...(C to CONTINUE) or (Q to Quit): ");
+            String proceedPrompt = scanner.nextLine();
+
+            if (proceedPrompt.equalsIgnoreCase("Q") || proceedPrompt.equalsIgnoreCase("QUIT")){
+                System.out.println("PROCESS ENDED!");
+                break;
+            }
+        } while(true);
     }
 }
