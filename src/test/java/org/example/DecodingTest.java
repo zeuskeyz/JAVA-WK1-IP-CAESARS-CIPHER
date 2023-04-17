e616d3c16cfb4f48262fd9d8b7b5e9f146fca1bf
zeus <zeuskeyz@gmail.com>
2023-04-17 14:06:18 +0300

    ammends code to the DecodingTest.java file to test f for key error functionality.

--- src/test/java/org/example/DecodingTest.java
+++ src/test/java/org/example/DecodingTest.java
@@ -10,5 +10,6 @@ class DecodingTest {
     @DisplayName(value = "TESTING DECODING FUNCTIONS")
     void decode() {
         assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 23));
+        assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 27));
     }
 }
