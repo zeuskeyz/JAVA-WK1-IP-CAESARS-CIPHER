d22ab0eeee062ed1b87d0e0cb02df44167b5e36a
zeus <zeuskeyz@gmail.com>
2023-04-17 13:50:18 +0300

    add code to the AlphabetsTest.java file to test f for Alphabet generation.

--- src/test/java/org/example/AlphabetsTest.java
+++ src/test/java/org/example/AlphabetsTest.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlphabetsTest {
-
     @Test
+    @DisplayName(value = "TESTING ALPHABET GENERATION")
     void getEncryptors() {
+        assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ",Alphabets.getEncryptors());
     }
 }
