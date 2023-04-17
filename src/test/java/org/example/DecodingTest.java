248d3a47145e7211c8cee3a95a8fb7e4bb2884e1
zeus <zeuskeyz@gmail.com>
2023-04-17 14:19:17 +0300

    ammends comments to the DecodingTest.java file and corrects some grammatical mistakes in the EncodingTest.java comments

--- src/test/java/org/example/DecodingTest.java
+++ src/test/java/org/example/DecodingTest.java
@@ -9,7 +9,11 @@ class DecodingTest {
     @Test
     @DisplayName(value = "TESTING DECODING FUNCTIONS")
     void decode() {
+
+        //TESTS IF THE USER INPUT IS ENCODED USING THE VALUE OF THE KEY PROVIDED
         assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 23));
+
+        //TESTS IF THERE IS AN ERROR THROWN IF THE KEY IS GREATER THAN OR LESS THAN THE LIMIT
         assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 27));
         assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 0));
     }
