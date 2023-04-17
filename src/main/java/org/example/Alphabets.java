65118e9619c8301ff67c739dab71e8abc43998a6
zeus <zeuskeyz@gmail.com>
2023-04-17 13:33:30 +0300

    adds to the Alphabets.java file, a function to generate the english alphabets and a getter to get the generated results

--- src/main/java/org/example/Alphabets.java
+++ src/main/java/org/example/Alphabets.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class Alphabets {
+    private static String encryptors (){
+        String encodingAlphabets = "";
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            encodingAlphabets += letter;
+        }
+        return encodingAlphabets;
+    }
+
+    public static String getEncryptors(){
+        return encryptors();
+    }
 }
