9523af7d2fce7a84c6be82eda9d8a312e3bab897
zeus <zeuskeyz@gmail.com>
2023-04-17 14:09:12 +0300

    ammends code to the EncodingTest.java file to test for key error functionality if key is greater than zero.

--- src/test/java/org/example/EncodingTest.java
+++ src/test/java/org/example/EncodingTest.java
@@ -10,5 +10,7 @@ class EncodingTest {
     @DisplayName(value = "TESTING ENCODING FUNCTIONS")
     void encode() {
         assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", Encoding.encode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23));
+        assertEquals("KEY ERROR! THE KEY HAS TO BE A NUMBER BETWEEN 1 AND 26\n", Decoding.decode("HELLO WORLD", 27));
+
     }
 }
